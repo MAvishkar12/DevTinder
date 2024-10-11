88dc5f70b53572f87b855397e96f821d3d359b69
MAvishkar12 <moreavishkar12@gamil.com>
2024-10-11 18:25:58 +0530

    Middleware

--- src/app.js
+++ src/app.js
@@ -1,20 +1,64 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
+// app.use("/",(req,res)=>{
+//   res.send("This is main location")
+// })
 
-app.use("/",(req,res)=>{
-  res.send("This is home of website page")
+// app.get("/user",(req,res)=>{
+//   res.send({"Firstname":"Ratan","LastName":"Tata"})
+// })
+
+// app.post("/user",(req,res)=>{
+//   res.send("This is post request")
+// })
+
+// app.delete("/user",(req,res)=>{
+//   res.send("This is delete http method method")
+// })
+
+// app.use(
+//   "/user",[
+//   (req, res, next) => {
+//     // console.log(req.params.userid);
+//     console.log("Response 1");
+
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Response 2");
+
+//     next();
+//   }],
+//   (req,res,next)=>{
+//     console.log("Response 3");
+//     res.send("Response 3")
+    
+//   },
+//   (req,res,next)=>{
+//     console.log("Response 4");
+//     res.send("Response 4")
+    
+//   }
+
+// );
+
+app.use("/user",(req,res,next)=>{
+  console.log("Response 1");
+  next()
 })
 
-app.use("/hello",(req,res)=>{
-  res.send("This is hello page")
+app.use("/user",(req,res,next)=>{
+  console.log("Response 2");
+ 
+  next();
+},
+(req,res,next)=>{
+  console.log("Response 3");
+  res.send("Response 3")
+     
 })
 
-app.use("/profile",(req,res)=>{
-  res.send("This is profile page")
-})
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Application start a port 3000");
 });
-
